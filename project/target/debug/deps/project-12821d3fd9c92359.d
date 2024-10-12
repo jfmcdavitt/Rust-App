85889ba8ae6438cbb0a72104609082aefe75fbc0
jfmcdavitt <mcdavittjacob@gmail.com>
2024-10-12 03:04:41 -0400

    Push

---
+++ project/target/debug/deps/project-12821d3fd9c92359.d
@@ -0,0 +1,5 @@
+/mnt/c/personal_repos/Rust-App/project/target/debug/deps/project-12821d3fd9c92359: src/main.rs
+
+/mnt/c/personal_repos/Rust-App/project/target/debug/deps/project-12821d3fd9c92359.d: src/main.rs
+
+src/main.rs:
---
+++ project/target/debug/project.d
@@ -0,0 +1,1 @@
+/mnt/c/personal_repos/Rust-App/project/target/debug/project: /mnt/c/personal_repos/Rust-App/project/src/main.rs
